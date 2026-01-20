d4ca57b359d9b7548c3992215e29944835aeb811
Jerzy Majka <jerzy.majka67.gmail.com>
2026-01-19 18:44:39 -0600

    Stable version before footer icon refactor

--- routes/en.js
+++ routes/en.js
@@ -1,46 +1,95 @@
 const express = require('express');
 const router = express.Router();
 
+// All routes for English pages
 router.get('/home', (req, res) => {
-  res.render('en/home', { title: 'Home - St. Joseph Parish', lang: 'en', page: 'home' });
+  res.render('en/home', { 
+    title: 'Home - Our Lady, Queen of Angels', 
+    lang: 'en', 
+    page: 'home',
+    favicon: '/images/logo-olqa-mini.png' // path to your mini icon
+  });
 });
 
 router.get('/about', (req, res) => {
-  res.render('en/about', { title: 'About - St. Joseph Parish', lang: 'en', page: 'about' });
+  res.render('en/about', { 
+    title: 'About - Our Lady, Queen of Angels', 
+    lang: 'en', 
+    page: 'about',
+    favicon: '/images/logo-olqa-mini.png'
+  });
 });
 
 router.get('/masses', (req, res) => {
-  res.render('en/masses', { title: 'Masses & Devotions - St. Joseph Parish', lang: 'en', page: 'masses' });
+  res.render('en/masses', { 
+    title: 'Masses & Devotions - Our Lady, Queen of Angels', 
+    lang: 'en', 
+    page: 'masses',
+    favicon: '/images/logo-olqa-mini.png'
+  });
 });
 
 router.get('/office', (req, res) => {
-  res.render('en/office', { title: 'Parish Office - St. Joseph Parish', lang: 'en', page: 'office' });
+  res.render('en/office', { 
+    title: 'Parish Office - Our Lady, Queen of Angels', 
+    lang: 'en', 
+    page: 'office',
+    favicon: '/images/logo-olqa-mini.png'
+  });
 });
 
 router.get('/bulletin', (req, res) => {
-  res.render('en/bulletin', { title: 'Bulletin - St. Joseph Parish', lang: 'en', page: 'bulletin' });
+  res.render('en/bulletin', { 
+    title: 'Bulletin - Our Lady, Queen of Angels', 
+    lang: 'en', 
+    page: 'bulletin',
+    favicon: '/images/logo-olqa-mini.png'
+  });
 });
 
 router.get('/groups', (req, res) => {
-  res.render('en/groups', { title: 'Parish Groups - St. Joseph Parish', lang: 'en', page: 'groups' });
+  res.render('en/groups', { 
+    title: 'Parish Groups - Our Lady, Queen of Angels', 
+    lang: 'en', 
+    page: 'groups',
+    favicon: '/images/logo-olqa-mini.png'
+  });
 });
 
 router.get('/communities', (req, res) => {
-  res.render('en/communities', { title: 'Chapels & Communities - St. Joseph Parish', lang: 'en', page: 'communities' });
+  res.render('en/communities', { 
+    title: 'Chapels & Communities - Our Lady, Queen of Angels', 
+    lang: 'en', 
+    page: 'communities',
+    favicon: '/images/logo-olqa-mini.png'
+  });
 });
 
 router.get('/homilies', (req, res) => {
-  res.render('en/homilies', { title: 'Homilies - St. Joseph Parish', lang: 'en', page: 'homilies' });
+  res.render('en/homilies', { 
+    title: 'Homilies - Our Lady, Queen of Angels', 
+    lang: 'en', 
+    page: 'homilies',
+    favicon: '/images/logo-olqa-mini.png'
+  });
 });
 
 router.get('/events', (req, res) => {
-  res.render('en/events', { title: 'Parish Events - St. Joseph Parish', lang: 'en', page: 'events' });
+  res.render('en/events', { 
+    title: 'Parish Events - Our Lady, Queen of Angels', 
+    lang: 'en', 
+    page: 'events',
+    favicon: '/images/logo-olqa-mini.png'
+  });
 });
 
 router.get('/contact', (req, res) => {
-  res.render('en/contact', { title: 'Contact Us - St. Joseph Parish', lang: 'en', page: 'contact' });
+  res.render('en/contact', { 
+    title: 'Contact Us - Our Lady, Queen of Angels', 
+    lang: 'en', 
+    page: 'contact',
+    favicon: '/images/logo-olqa-mini.png'
+  });
 });
 
 module.exports = router;
-
-
--- routes/es.js
+++ routes/es.js
@@ -1,54 +1,107 @@
 const express = require('express');
 const router = express.Router();
 
+// Path to favicon
+const faviconPath = '/images/logo-olqa-mini.png';
+
 // HOME
 router.get('/inicio', (req, res) => {
-  res.render('es/inicio', { title: 'Inicio - Parroquia San José', lang: 'es', page: 'home' });
+  res.render('es/inicio', { 
+    title: 'Inicio - Nuestra Señora Reyna de Los Ángeles', 
+    lang: 'es', 
+    page: 'home', 
+    favicon: faviconPath
+  });
 });
 
 // ABOUT
 router.get('/sobre', (req, res) => {
-  res.render('es/sobre', { title: 'Sobre la Parroquia - Parroquia San José', lang: 'es', page: 'about' });
+  res.render('es/sobre', { 
+    title: 'Sobre la Parroquia - Nuestra Señora Reyna de Los Ángeles', 
+    lang: 'es', 
+    page: 'about', 
+    favicon: faviconPath
+  });
 });
 
 // MASSES & DEVOTIONS
 router.get('/misas', (req, res) => {
-  res.render('es/misas', { title: 'Misas y Devociones - Parroquia San José', lang: 'es', page: 'masses' });
+  res.render('es/misas', { 
+    title: 'Misas y Devociones - Nuestra Señora Reyna de Los Ángeles', 
+    lang: 'es', 
+    page: 'masses', 
+    favicon: faviconPath
+  });
 });
 
 // PARISH OFFICE
 router.get('/oficina', (req, res) => {
-  res.render('es/oficina', { title: 'Oficina Parroquial - Parroquia San José', lang: 'es', page: 'office' });
+  res.render('es/oficina', { 
+    title: 'Oficina Parroquial - Nuestra Señora Reyna de Los Ángeles', 
+    lang: 'es', 
+    page: 'office', 
+    favicon: faviconPath
+  });
 });
 
 // BULLETIN
 router.get('/boletin', (req, res) => {
-  res.render('es/boletin', { title: 'Boletín Parroquial - Parroquia San José', lang: 'es', page: 'bulletin' });
+  res.render('es/boletin', { 
+    title: 'Boletín Parroquial - Nuestra Señora Reyna de Los Ángeles', 
+    lang: 'es', 
+    page: 'bulletin', 
+    favicon: faviconPath
+  });
 });
 
 // PARISH GROUPS
 router.get('/grupos', (req, res) => {
-  res.render('es/grupos', { title: 'Grupos Parroquiales - Parroquia San José', lang: 'es', page: 'groups' });
+  res.render('es/grupos', { 
+    title: 'Grupos Parroquiales - Nuestra Señora Reyna de Los Ángeles', 
+    lang: 'es', 
+    page: 'groups', 
+    favicon: faviconPath
+  });
 });
 
 // CHAPELS & COMMUNITIES
 router.get('/comunidades', (req, res) => {
-  res.render('es/comunidades', { title: 'Capillas y Comunidades - Parroquia San José', lang: 'es', page: 'communities' });
+  res.render('es/comunidades', { 
+    title: 'Capillas y Comunidades - Nuestra Señora Reyna de Los Ángeles', 
+    lang: 'es', 
+    page: 'communities', 
+    favicon: faviconPath
+  });
 });
 
 // HOMILIES
 router.get('/homilias', (req, res) => {
-  res.render('es/homilias', { title: 'Homilías - Parroquia San José', lang: 'es', page: 'homilies' });
+  res.render('es/homilias', { 
+    title: 'Homilías - Nuestra Señora Reyna de Los Ángeles', 
+    lang: 'es', 
+    page: 'homilies', 
+    favicon: faviconPath
+  });
 });
 
 // EVENTS
 router.get('/eventos', (req, res) => {
-  res.render('es/eventos', { title: 'Eventos Parroquiales - Parroquia San José', lang: 'es', page: 'events' });
+  res.render('es/eventos', { 
+    title: 'Eventos Parroquiales - Nuestra Señora Reyna de Los Ángeles', 
+    lang: 'es', 
+    page: 'events', 
+    favicon: faviconPath
+  });
 });
 
 // CONTACT
 router.get('/contacto', (req, res) => {
-  res.render('es/contacto', { title: 'Contacto - Parroquia San José', lang: 'es', page: 'contact' });
+  res.render('es/contacto', { 
+    title: 'Contacto - Nuestra Señora Reyna de Los Ángeles', 
+    lang: 'es', 
+    page: 'contact', 
+    favicon: faviconPath
+  });
 });
 
 module.exports = router;
